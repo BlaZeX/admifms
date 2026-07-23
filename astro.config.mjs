d09ef40a6ec8b5b3ab568c1eb4ffb775c396b792
Shashank Tiwari <stiwarihq@gmail.com>
2026-07-23 18:31:15 +0000

    Updated MMS page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://admi.mu.ac.in',
   output: 'static',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  vite: {
+    server: {
+      allowedHosts: ['code.tiwari.co']
+    }
+  }
 });
